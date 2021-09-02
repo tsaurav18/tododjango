7dcc3528e082f50e3a47d8999862c6fa3e313ab3
tsaurav18 <tsaurav1711@gmail.com>
2021-09-02 23:55:23 +0900

    display all the today list

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-
+from .models import Todo
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
-
-
-
-        
-        
-
-
-
-
+    todos_list = Todo.objects.all()
+    return render(request, 'index.html',{"todos":todos_list})
+     
